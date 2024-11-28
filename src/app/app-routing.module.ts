a56b8617cac8b68fda4f205c65aad267d4085fa4
NRDJ <nriedeldj@gmail.com>
2024-11-28 14:25:03 -0300

    Agregar navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'login' }
-
+  { path: 'navbar', component: NavbarComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
